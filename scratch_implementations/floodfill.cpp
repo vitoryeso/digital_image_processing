de1b5572db848a62259a4d5c8209a106521aa26b
vitoryeso <vitoryeso@outlook.com>
2020-10-14 12:57:59 -0300

    starting floodfill from scratch

---
+++ scratch_implementations/floodfill.cpp
@@ -0,0 +1,34 @@
+#include "opencv2/highgui.hpp"
+#include "opencv2/imgcodecs.hpp"
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+using namespace std;
+
+void floodfill(cv::Mat& M, cv::Point, int label);
+
+cv::Mat M;
+
+static void onMouse(int event, int x, int y, int flags, void* param) {
+  if(event == cv::EVENT_LBUTTONDOWN) {
+    unsigned b, g, r;
+    b = M.at<cv::Vec3b>(y, x)[0];
+    g = M.at<cv::Vec3b>(y, x)[1];
+    r = M.at<cv::Vec3b>(y, x)[2];
+    cout << "x: " << x << ", y: " << y << ", rgb: (" << r << ", " << g << ", " << b << ")" << endl;
+  }
+}
+
+int main(int argc, char** argv) {
+  M = cv::imread(argv[1], cv::IMREAD_COLOR);
+  
+  cv::namedWindow("img", 0);
+  cv::setMouseCallback("img", onMouse, 0);
+  while(1) {
+    cv::imshow("img", M);
+    char c = (char)cv::waitKey();
+    if(c == 27) break;
+  }
+
+}
+
